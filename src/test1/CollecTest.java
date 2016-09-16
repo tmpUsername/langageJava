ce77a0ac6f0252ab52be6fde8cf8b731115eba4a
tmpUsername <admin@fddf-PC>
2016-09-16 10:40:33 +0200

    CollecTest.java: petit test sur les ensembles et les listes

--- src/test1/CollecTest.java
+++ src/test1/CollecTest.java
@@ -5,6 +5,9 @@
  */
 package test1;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 /**
  *
  * @author admin
@@ -12,6 +15,29 @@ package test1;
 public class CollecTest {
     
     public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+        ArrayList<Integer> liste = new ArrayList<>();
+        
+        //ajout à l'ensemble
+        for (int i = 1; i < 4; i++) {
+            set.add(i);
+        }
+        
+        //ajout à la liste
+        for (int i = 1; i < 7; i++) {
+            liste.add(i);
+        }
+        for (int i = 3; i < 5; i++){
+            liste.add(i);
+        }
+        
+        //ajout de l'ensemble à la liste
+        liste.addAll(set);
+        System.out.println(liste);
+        
+        //ajout de la liste à l'ensemble
+        set.addAll(liste);
+        System.out.println(set);
         
     }
 }
