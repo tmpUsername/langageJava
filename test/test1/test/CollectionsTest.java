38d6507051a5eff4f77b42524546291b92be883b
tmpUsername <admin@fddf-PC>
2016-09-16 12:06:11 +0200

    CollectionsTest.java: Modification de creerNouvMap pour faire le test correct

--- test/test1/test/CollectionsTest.java
+++ test/test1/test/CollectionsTest.java
@@ -6,6 +6,7 @@
 package test1.test;
 
 import java.util.HashMap;
+import java.util.Map;
 import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -15,54 +16,61 @@ import static org.junit.Assert.*;
  * @author admin
  */
 public class CollectionsTest {
-    
+
     @Test
-    public void recupRuby(){
+    public void recupRuby() {
         HashMap<String, String> map = new HashMap<>();
-        
+
         map.put("Java", "Langage de programmation objet");
         map.put("C", "Langage de programmation structuré");
         map.put("Ruby", "Langage de script Objet");
-        
-        Assert.assertSame(map.get("Ruby"),"Langage de script Objet");
+
+        Assert.assertSame(map.get("Ruby"), "Langage de script Objet");
     }
-    
+
     @Test
-    public void remplaceDefRubyParSuperLangage(){
+    public void remplaceDefRubyParSuperLangage() {
         HashMap<String, String> map = new HashMap<>();
-        
+
         map.put("Java", "Langage de programmation objet");
         map.put("C", "Langage de programmation structuré");
         map.put("Ruby", "Langage de script Objet");
-        
+
         map.replace("Ruby", "Super langage");
-        
+
         Assert.assertSame("Super langage", map.get("Ruby"));
     }
-    
+
     @Test
     public void supprimeLangageC() {
         HashMap<String, String> map = new HashMap<>();
-        
+
         map.put("Java", "Langage de programmation objet");
         map.put("C", "Langage de programmation structuré");
         map.put("Ruby", "Langage de script Objet");
-        
+
         map.remove("C");
-        
+
         Assert.assertFalse(map.containsKey("C"));
     }
-    
+
     @Test
-    public void CreerNouvMap(){
+    public void CreerNouvMap() {
         HashMap<String, String> map = new HashMap<>();
-        
+
         map.put("PHP", "Langage de programmation web");
         map.put("Python", "Langage de script");
-        
-        Assert.assertTrue(map.containsKey("PHP"));
-        Assert.assertTrue(map.containsKey("Python"));
+
+        HashMap<String, String> map1 = new HashMap<>();
+
+        map1.put("Java", "Langage de programmation objet");
+        map1.put("C", "Langage de programmation structuré");
+        map1.put("Ruby", "Langage de script Objet");
+
+        map.putAll(map1);
+
+        for (Map.Entry<String, String> elem : map1.entrySet()) {
+            Assert.assertTrue(map.containsKey(elem.getKey()));
+        }
     }
-    
-    
 }
