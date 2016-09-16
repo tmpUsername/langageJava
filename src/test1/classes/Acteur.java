8390e46e1607b307ff4f60faae46f3a35622ecf1
tmpUsername <admin@fddf-PC>
2016-09-16 14:38:09 +0200

    Acteur.java: Ajout des accesseurs

--- src/test1/classes/Acteur.java
+++ src/test1/classes/Acteur.java
@@ -17,5 +17,21 @@ public class Acteur {
     ////CONSTRUCTOR
     
     ////ACCESSORS
-    
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    ////METHODS
 }
