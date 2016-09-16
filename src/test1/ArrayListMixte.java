2474e87c0850eb94962297ca907f579462252b86
tmpUsername <admin@fddf-PC>
2016-09-16 10:02:37 +0200

    ArrayListMixte.java: Exercice sur les listes

--- src/test1/ArrayListMixte.java
+++ src/test1/ArrayListMixte.java
@@ -5,6 +5,8 @@
  */
 package test1;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author admin
@@ -12,7 +14,30 @@ package test1;
 public class ArrayListMixte {
     
     public static void main(String[] args) {
+        //Declaration
+        ArrayList liste = new ArrayList();
+        ArrayList<Integer> filtre = new ArrayList<>();
         
+        //affectation des valeurs à la liste
+        liste.add('A');
+        liste.add('B');
+        liste.add(1);
+        liste.add(true);
+        liste.add("Coucou");
+        liste.add(4);
+        liste.add(5);
+        liste.add(6);
+        
+        //affectation des valeurs à filtrer
+        filtre.add(4);
+        filtre.add(5);
+        filtre.add(6);
+        
+        //Filtrage de la liste avec le filtre
+        liste.removeAll(filtre);
+        
+        //impression
+        System.out.println(liste);
     }
     
 }
