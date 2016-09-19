515599a59c825012e6c1d0c41aaa8c34113d2993
tmpUsername <admin@fddf-PC>
2016-09-19 15:51:26 +0200

    Tortue.java: implementation de la classe tortue

---
+++ src/test1/classes/animaux/Tortue.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1.classes.animaux;
+
+/**
+ *
+ * @author admin
+ */
+public class Tortue extends Reptile{
+
+    public Tortue(String nom) {
+        super(nom);
+    }
+
+    @Override
+    public void manger() {
+        System.out.println(this.getNom() + " mâche");
+    }
+
+    @Override
+    public void bouger() {
+        System.out.println(this.getNom() + " marche quand je suis sur terre");
+    }
+    
+}
