42e4daea88185d55c7538b66b9ca5ec3f64e31b9
tmpUsername <admin@fddf-PC>
2016-09-19 09:59:26 +0200

    Gateau.java: ajout de toString

---
+++ src/test1/gateau/Gateau.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1.gateau;
+
+/**
+ *
+ * @author admin
+ */
+public class Gateau {
+    
+    ////ATTRIBUTS
+    private String forme;
+    private String gout;
+    
+    ////ACCESSORS
+    public String getForme() {
+        return forme;
+    }
+
+    public void setForme(String forme) {
+        this.forme = forme;
+    }
+
+    public String getGout() {
+        return gout;
+    }
+
+    public void setGout(String gout) {
+        this.gout = gout;
+    }
+    
+    ////METHODS
+
+    @Override
+    public String toString() {
+        return "Gateau au " + this.gout;
+    }
+    
+}
