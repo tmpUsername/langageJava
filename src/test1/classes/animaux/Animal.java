edf88dce0ec838b9d680d5512f70fec3b2c43aa0
tmpUsername <admin@fddf-PC>
2016-09-19 14:56:49 +0200

    Animal.java: ajout d'une classe abstraite animale

---
+++ src/test1/classes/animaux/Animal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1.classes.animaux;
+
+/**
+ *
+ * @author admin
+ */
+abstract public class Animal {
+    ////ATTRIBUTS
+    private String nom;
+    
+    ////METHODS
+    abstract public void manger();
+    abstract public void bouger();
+}
