e55c6ef953ac50eca9ea1ec9be8c5ca2d5cfccf2
tmpUsername <admin@fddf-PC>
2016-09-19 15:07:13 +0200

    Poisson.java: implementation des methods abstract

--- src/test1/classes/animaux/Poisson.java
+++ src/test1/classes/animaux/Poisson.java
@@ -13,12 +13,12 @@ public class Poisson extends Animal{
 
     @Override
     public void manger() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println(this.getNom() + " gobe.");
     }
 
     @Override
     public void bouger() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println(this.getNom() + " nage.");
     }
     
 }
