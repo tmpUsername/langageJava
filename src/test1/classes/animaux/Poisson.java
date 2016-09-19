8f4e10f08c5835cd845faffb4cb3a16269ddc7d6
tmpUsername <admin@fddf-PC>
2016-09-19 15:01:31 +0200

    Poisson.java: ajout de la classe Poisson

---
+++ src/test1/classes/animaux/Poisson.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1.classes.animaux;
+
+/**
+ *
+ * @author admin
+ */
+public class Poisson extends Animal{
+
+    @Override
+    public void manger() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void bouger() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
