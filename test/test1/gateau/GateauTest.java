65e471c1a8cb0dadde74581ab37aa132c1b42e87
tmpUsername <admin@fddf-PC>
2016-09-19 09:53:07 +0200

    GateauTest.java: ajout d'une classe de test

---
+++ test/test1/gateau/GateauTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1.gateau;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author admin
+ */
+public class GateauTest {
+    
+    public GateauTest() {
+    }
+    
+}
