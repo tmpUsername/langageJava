79e2d28f3fb5828477741b58ea90f116ae133470
tmpUsername <admin@fddf-PC>
2016-09-19 15:13:08 +0200

    AnimalTest.java: ajout de test sur les poissons

---
+++ test/test1/classes/animaux/test/AnimalTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1.classes.animaux.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import test1.classes.animaux.Poisson;
+
+/**
+ *
+ * @author admin
+ */
+public class AnimalTest {
+    
+    @Test
+    public void testouillage(){
+        Poisson p = new Poisson();
+        
+        p.bouger();
+        p.manger();
+    }
+    
+}
